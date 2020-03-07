7a99a30e4e42b117f04c9c7aca5828d94a4dd380
dante-mo <57079987+dante-m@users.noreply.github.com>
2020-03-07 17:17:53 -0600

    Update app.py

--- main code/app.py
+++ main code/app.py
@@ -31,12 +31,7 @@ results = db.execute("SELECT * FROM baseballdata_table")
 
 ####  html routes ###################################
 
-# @app.route("/")
-# def index():
-    
-#     return render_template('index.html')
-
-@app.route("/index.html")
+@app.route("/")
 def index():
     
     return render_template('index.html')
