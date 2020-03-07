ea5ee6ccd6ad0b33da2e28e9c11c67edc7d625e5
dante-mo <57079987+dante-m@users.noreply.github.com>
2020-03-07 09:20:27 -0600

    Delete join schema.sql

--- dantes stuff/data/join schema.sql
+++
@@ -1,4 +0,0 @@
-SELECT kaggle_data.oteam_name, kaggle_data.year, Kaggle_data.lg, Kaggle_data.w, Kaggle_data.l, Kaggle_data.t,Kaggle_data.w_l,Kaggle_data.playoffs, Kaggle_data.att, Kaggle_data.cteam_name, stadiums_csv.team, stadiums_csv.venue,stadiums_csv.address,stadiums_csv.city, stadiums_csv.lat, stadiums_csv.lng 
-from kaggle_data
-INNER JOIN stadiums_csv
-ON kaggle_data.cteam_name = stadiums_csv.team;
